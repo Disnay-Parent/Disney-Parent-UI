f9640aa5bcc4379415092f62586ba06e67629783
Brittany Fisher-Morris <bmmf@bms-imac.attlocal.net>
2019-10-23 03:58:05 -0400

    navigation bar reworked. Desktop view working, hambgurlar not.

---
+++ js/index.js
@@ -0,0 +1,9 @@
+const toggleButton = document.getElementsByClassName('toggle-button')[0]
+console.log(toggleButton);
+
+const navbarLinks = document.getElementsByClassName('navbar-links')[0]
+console.log(navbarLinks)
+
+toggleButton.addEventListener('click', () => {
+    navbarLinks.classList.toggle('active')
+})
